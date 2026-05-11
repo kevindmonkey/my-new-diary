8dc263ce66f144f54b83128bad35e10f39b431fe
Athrone Yael F Rosal <athroneyael.rosal@college.cksc.edu.ph>
2026-05-11 08:39:10 +0800

    - Added more DiaryEntryTest.kt - Fixed some problems DiaryEntryTest.kt
    Signed-off-by: Athrone Yael F Rosal <athroneyael.rosal@college.cksc.edu.ph>

--- app/src/androidTest/java/ph/edu/cksc/college/appdev/mydiary/screens/DiaryEntryTest.kt
+++ app/src/androidTest/java/ph/edu/cksc/college/appdev/mydiary/screens/DiaryEntryTest.kt
@@ -1,123 +1,147 @@
 package ph.edu.cksc.college.appdev.mydiary.screens
 
-
 import android.annotation.SuppressLint
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.onParent
-import androidx.compose.ui.test.performClick
-import androidx.compose.ui.test.performTextReplacement
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import ph.edu.cksc.college.appdev.mydiary.components.DiaryEntryComponent
 import ph.edu.cksc.college.appdev.mydiary.components.DiaryEntryViewModel
 import ph.edu.cksc.college.appdev.mydiary.diary.DiaryEntry
+import ph.edu.cksc.college.appdev.mydiary.service.StorageService
+import ph.edu.cksc.college.appdev.mydiary.supabase
+import ph.edu.cksc.college.appdev.mydiary.ui.theme.MyDiaryTheme
 import java.time.LocalDateTime
 
+/**
+ * UI Tests for the Diary Entry screen.
+ * Verifies that entry details are displayed correctly and can be edited.
+ */
 class DiaryEntryTest {
 
     @get:Rule
     val composeTestRule = createComposeRule()
-    // use createAndroidComposeRule<YourActivity>() if you need access to
-    // an activity
 
     @Before
     fun setUp() {
-        // Start the app
         composeTestRule.setContent {
-            DiaryEntryComponent(viewModel = object: DiaryEntryViewModel {
-                @SuppressLint("UnrememberedMutableState")
-                override var diaryEntry = mutableStateOf(DiaryEntry())
-                init {
-                    diaryEntry.value = DiaryEntry(
-                        "0",
-                        0,
-                        1,
-                        "Lexi",
-                        "Test...Test...Test...",
-                        LocalDateTime.of(2026, 1, 1, 7, 30).toString()
-                    )
-                }
-                override var modified: Boolean = true
+            val storageService = StorageService(supabase)
+            MyDiaryTheme {
+                DiaryEntryComponent(
+                    id = "test-id",
+                    viewModel = object : DiaryEntryViewModel {
+                        @SuppressLint("UnrememberedMutableState")
+                        override var diaryEntry = mutableStateOf(
+                            DiaryEntry(
+                                id = "test-id",
+                                mood = 0, // Happy
+                                star = 1,
+                                title = "Lexi",
+                                content = "Test Content",
+                                dateTime = LocalDateTime.of(2026, 1, 1, 7, 30).toString()
+                            )
+                        )
+                        override var modified: Boolean = false
 
-                override fun onTitleChange(newValue: String) {
-                    diaryEntry.value = diaryEntry.value.copy(title = newValue)
-                }
-                override fun onContentChange(newValue: String) {
-                    diaryEntry.value = diaryEntry.value.copy(content = newValue)
-                }
-                override fun onMoodChange(newValue: Int) {
-                    diaryEntry.value = diaryEntry.value.copy(mood = newValue)
-                }
-                override fun onStarChange(newValue: Int) {
-                    diaryEntry.value = diaryEntry.value.copy(star = newValue)
-                }
-                override fun onDateTimeChange(newValue: LocalDateTime) {
-                    val newDueDate = newValue.toString()
-                    diaryEntry.value = diaryEntry.value.copy(dateTime = newDueDate)
-                }
-                override fun onDoneClick(popUpScreen: () -> Unit) {
+                        override fun onTitleChange(newValue: String) {
+                            diaryEntry.value = diaryEntry.value.copy(title = newValue)
+                        }
 
-                }
-            }, test = false)
+                        override fun onContentChange(newValue: String) {
+                            diaryEntry.value = diaryEntry.value.copy(content = newValue)
+                        }
+
+                        override fun onMoodChange(newValue: Int) {
+                            diaryEntry.value = diaryEntry.value.copy(mood = newValue)
+                        }
+
+                        override fun onStarChange(newValue: Int) {
+                            diaryEntry.value = diaryEntry.value.copy(star = newValue)
+                        }
+
+                        override fun onLocationChange(newValue: String) {
+                            diaryEntry.value = diaryEntry.value.copy(location = newValue)
+                        }
+
+                        override fun onDateTimeChange(newValue: LocalDateTime) {
+                            diaryEntry.value = diaryEntry.value.copy(dateTime = newValue.toString())
+                        }
+
+                        override fun onAddPhoto(url: String) {}
+                        override fun onRemovePhoto(url: String) {}
+                        override fun onAddVoiceMemo(url: String) {}
+                        override fun onRemoveVoiceMemo(url: String) {}
+                        override fun onDoneClick(popUpScreen: () -> Unit) {}
+                    },
+                    storageService = storageService,
+                    isEditing = true,
+                    onDateClick = {},
+                    onCancel = {},
+                    onSave = {}
+                )
+            }
         }
     }
 
     @Test
-    fun testMoodDisplayed() {
-        composeTestRule.onNodeWithText("Mood").assertIsDisplayed()
+    fun testMoodAndStarsDisplayed() {
+        // Assert initial mood and star rating chips are shown
         composeTestRule.onNodeWithText("Happy").assertIsDisplayed()
+        composeTestRule.onNodeWithText("1 Stars").assertIsDisplayed()
     }
 
     @Test
-    fun testStarDisplayed() {
-        composeTestRule.onNodeWithText("Star").assertIsDisplayed()
-        composeTestRule.onNodeWithText("1").assertIsDisplayed()
-    }
-
-    @Test
-    fun testTitleDisplayed() {
-        composeTestRule.onNodeWithText("Title").assertIsDisplayed()
+    fun testTitleAndContentDisplayed() {
+        // Assert title and content text fields show the correct values
         composeTestRule.onNodeWithText("Lexi").assertIsDisplayed()
-    }
-
-    @Test
-    fun testContentDisplayed() {
-        composeTestRule.onNodeWithText("Content").assertIsDisplayed()
-        composeTestRule.onNodeWithText("Test...Test...Test...").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Test Content").assertIsDisplayed()
     }
 
     @Test
     fun testDateTimeDisplayed() {
-        composeTestRule.onNodeWithText("Jan 1, 26\n7:30 AM").assertIsDisplayed()
+        // Assert date and time are formatted correctly
+        // Jan 1, 2026 is a Thursday
+        composeTestRule.onNodeWithText("THURSDAY, JANUARY 1, 2026").assertIsDisplayed()
+        composeTestRule.onNodeWithText("7:30 AM").assertIsDisplayed()
     }
 
     @Test
-    fun testTitleCanBeEdited() {
-        composeTestRule.onNodeWithText("Title").assertIsDisplayed()
-        composeTestRule.onNodeWithText("Title", useUnmergedTree = true).onParent()
-            .performTextReplacement("New title")
-        //composeTestRule.waitUntilDoesNotExist(hasText("Lexi"))
-        composeTestRule.onNodeWithText("New title").assertIsDisplayed()
+    fun testTitleEditing() {
+        // Test replacing text in the title field
+        composeTestRule.onNodeWithText("Lexi").performTextReplacement("Updated Title")
+        composeTestRule.onNodeWithText("Updated Title").assertIsDisplayed()
     }
 
     @Test
-    fun testContentCanBeEdited() {
-        composeTestRule.onNodeWithText("Content").assertIsDisplayed()
-        composeTestRule.onNodeWithText("Content", useUnmergedTree = true).onParent()
-            .performTextReplacement("New content")
-        //composeTestRule.waitUntilDoesNotExist(hasText("Test...Test...Test..."))
-        composeTestRule.onNodeWithText("New content").assertIsDisplayed()
+    fun testContentEditing() {
+        // Test replacing text in the content field
+        composeTestRule.onNodeWithText("Test Content").performTextReplacement("Updated Content Body")
+        composeTestRule.onNodeWithText("Updated Content Body").assertIsDisplayed()
     }
 
     @Test
-    fun testMoodCanBeChanged() {
-        composeTestRule.onNodeWithText("Mood").performClick()
+    fun testMoodChanging() {
+        // Open mood dropdown
+        composeTestRule.onNodeWithText("Happy").performClick()
+        
+        // Select "Angry"
+        composeTestRule.onNodeWithText("Angry").performClick()
+        
+        // Verify UI updated
         composeTestRule.onNodeWithText("Angry").assertIsDisplayed()
-        composeTestRule.onNodeWithText("Love").performClick()
-        composeTestRule.onNodeWithText("Love").assertIsDisplayed()
+    }
+
+    @Test
+    fun testStarRatingChanging() {
+        // Open stars dropdown
+        composeTestRule.onNodeWithText("1 Stars").performClick()
+        
+        // Select "5 Stars"
+        composeTestRule.onNodeWithText("5 Stars").performClick()
+        
+        // Verify UI updated
+        composeTestRule.onNodeWithText("5 Stars").assertIsDisplayed()
     }
 }
